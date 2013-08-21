2cd18f5e7f6b6872504e3f983a219e079e5d8c79
Jeremy Jones <jmjone@illinois.edu>
2013-08-21 14:43:33 -0500

    Initial commit (working)

---
+++ config.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+    "io/ioutil"
+    "os"
+    "encoding/xml"
+)
+
+type Output struct {
+    Name string `xml:"name,attr"`
+    Port int `xml:"port,attr"`
+    WallID int `xml:"wallid,attr"`
+}
+
+type Input struct {
+    Name string `xml:"name,attr"`
+    Port int `xml:"port,attr"`
+}
+
+type Config struct {
+    Inputs []Input `xml:"inputs>input"`
+    Outputs []Output `xml:"outputs>output"`
+}
+
+type Panel struct {
+    Name string `xml:"name,attr"`
+    Source string `xml:"source"`
+}
+
+type WInput struct {
+    Name string `xml:"name,attr"`
+    Outputs []string `xml:"output"`
+}
+
+type WPanel struct {
+    Position int `xml:"pos,attr"`
+    Name string `xml:"name,attr"`
+}
+
+type WSize struct {
+    X int `xml:"x,attr"`
+    Y int `xml:"y,attr"`
+}
+
+type Wall struct {
+    Size WSize `xml:"size"`
+    Panels []WPanel `xml:"panel"`
+}
+
+type Layout struct {
+    Name string `xml:"name,attr"`
+    PanelConfigs []Panel `xml:"panelconfig>panel"`
+    VideoSwitch []WInput `xml:"videoswitch>input"`
+    VideoWalls []Wall `xml:"videowall>wall"`
+}
+
+type Configuration struct {
+    Config Config `xml:"config"`
+    Layouts []Layout `xml:"layouts>layout"`
+}
+
+func parseConfig(cfgfile string) (*Configuration, error) {
+    cfile, err := os.Open(cfgfile)
+    if err != nil {
+        return nil, err
+    }
+    defer cfile.Close()
+    cfgbuf, _ := ioutil.ReadAll(cfile)
+
+    var config Configuration
+    err = xml.Unmarshal(cfgbuf, &config)
+    if err != nil {
+        return nil, err
+    }
+    return &config, nil
+}
